c1d030b9db9a4a00800adbd4c0484a29cb789bb2
Raffalea <shaquilleraffalea@gmail.com>
2025-09-13 16:44:04 +0700

    menambahkan file file category

---
+++ src/app/Service/category-product.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = 'https://localhost:7152/api/Categories'; // Ganti dengan URL API yang sesuai
+
+  constructor(private http: HttpClient) { }
+  getAll(): Observable<any> {
+    return this.http.get(this.apiUrl+"/GetAll");
+  }
+  createData(model: any): Observable<any> {
+    const headers = new HttpHeaders ({'Content-Type': 'application/json' });
+    const options = { headers: headers };
+    return this.http.post(this.apiUrl+"/CreateData", model, options);
+  }
+
+  getById(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl + "/GetById"}/${id}`);
+  }
+
+  updateData(id: string, model: any): Observable<any> {
+    const headers = new HttpHeaders ({'Content-Type': 'application/json' });
+    const options = { headers: headers };
+    return this.http.put(`${this.apiUrl + "/UpdateData"}/${id}`, model, options);
+  }
+
+  deleteData(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl + "/DeleteData"}/${id}`);
+  }
+
+}
---
+++ src/app/category-product/category-product.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-category-product',
+  standalone: false,
+  templateUrl: './category-product.html',
+  styleUrl: './category-product.css'
+})
+export class CategoryProduct {
+
+}
